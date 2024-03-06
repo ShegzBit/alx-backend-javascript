1be03797494751b2298018981aea5895f4ee022a
ShegzBit <feranmiawotubo16@gmail.com>
2024-03-06 03:41:37 +0100

    fixed task 3 [2]

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { createUser, uploadPhoto } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([createUser(), uploadPhoto()])
     .then(([user, photo]) => {
-      console.log(`${photo.body} ${user.firstname} ${user.lastname}`);
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
