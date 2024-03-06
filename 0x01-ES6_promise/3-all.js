66d4f678ac9e75faffcc5e2df70b2f16cdc62539
ShegzBit <feranmiawotubo16@gmail.com>
2024-03-06 03:35:37 +0100

    fixed task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,6 +6,6 @@ export default function handleProfileSignup() {
       console.log(`${photo.body} ${user.firstname} ${user.lastname}`);
     })
     .catch(() => {
-      console.error('Signup system offline');
+      console.log('Signup system offline');
     });
 }
