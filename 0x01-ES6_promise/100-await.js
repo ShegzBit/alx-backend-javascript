a7fa84fc75672e743579bc6de2ce4aee34b616c0
ShegzBit <feranmiawotubo16@gmail.com>
2024-03-06 22:43:42 +0100

    fixed task 10[2]

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,7 +2,7 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   try {
-    return { photo: uploadPhoto(), user: await createUser() };
+    return { photo: await uploadPhoto(), user: await createUser() };
   } catch (err) {
     return { photo: null, user: null };
   }
