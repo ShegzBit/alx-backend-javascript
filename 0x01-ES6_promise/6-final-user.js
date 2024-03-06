13640ca95c482e02e1fc9124ed9b8a8c3163ad5b
ShegzBit <feranmiawotubo16@gmail.com>
2024-03-06 21:26:13 +0100

    solved task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,9 +2,11 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
-  return promises.map((result) => {
-    const value = (result.status === 'fulfilled') ? result.value : result.reason;
-    return { status: result.status, value };
-  });
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => {
+      res.map((obj) => {
+        const value = (obj.status === 'fulfilled') ? res.value : res.reason;
+        return { status: obj.status, value };
+      });
+    });
 }
