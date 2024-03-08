c29c37f69be844daa4fd7aaf7e0f6f219e099dff
ShegzBit <feranmiawotubo16@gmail.com>
2024-03-08 20:42:51 +0100

    solved task 13

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,2 +1,19 @@
-export default function createIteratorObject() {
+export default function createIteratorObject(report) {
+  const keys = Object.getOwnPropertyNames(report.allEmployees);
+  const departmentList = report.allEmployees;
+  return {
+    ...report,
+    [Symbol.iterator]() {
+      return {
+        currentKey: keys.shift(),
+        next() {
+          if (departmentList[this.currentKey].length === 0) { this.currentKey = keys.shift(); }
+          if (!this.currentKey) {
+            return { value: undefined, done: true };
+          }
+          return { value: departmentList[this.currentKey].shift(), done: false };
+        },
+      };
+    },
+  };
 }
