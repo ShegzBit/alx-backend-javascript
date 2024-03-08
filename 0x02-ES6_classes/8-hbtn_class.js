b53bd8b1a518079e7104b7d7e17d7e8e3d13086a
ShegzBit <feranmiawotubo16@gmail.com>
2024-03-08 23:25:27 +0100

    solved task 8

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  toString() { return this._location; }
+
+  valueOf() { return this._size; }
+}
