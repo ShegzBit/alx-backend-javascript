3b0c630635272bfb8d48a4b0763c3c0adba2de64
ShegzBit <feranmiawotubo16@gmail.com>
2024-03-09 09:13:34 +0100

    solved task 4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,1 +1,24 @@
-#!/usr/bin/node
+import Currency from './3-currency';
+
+export default class Pricing {
+  static raiseError(key) {
+    if (key === 'amount') { throw new Error('Amount must be a number'); } else if (key === 'currency') { throw new Error('Currency must be a Currency'); }
+  }
+
+  constructor(amount, currency) {
+    this._amount = (typeof (amount) === 'number') ? amount : Pricing.raiseError('amount');
+    this._currency = (currency instanceof Currency) ? currency : Pricing.raiseError('currency');
+  }
+
+  get amount() { return this._amount; }
+
+  set amount(val) { this._amount = (typeof (val) === 'number') ? val : Pricing.raiseError('amount'); }
+
+  get currency() { return this._currency; }
+
+  set currency(val) { this._currency = (val instanceof Currency) ? val : Pricing.raiseError('currency'); }
+
+  displayFullPrice() { return `${this._amount} ${this._currency.displayFullCurrency()}`; }
+
+  static convertPrice(amount, conversionRate) { return amount * conversionRate; }
+}
