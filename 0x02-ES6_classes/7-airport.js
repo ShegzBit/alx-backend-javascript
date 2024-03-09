ab4f5ef464ed3d09c2a3578deb8e50ca8a8ab6ed
ShegzBit <feranmiawotubo16@gmail.com>
2024-03-09 09:25:55 +0100

    solved task 7

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+export default class Airport {
+  constructor(name, code) {
+    this._code = code;
+    this._name = name;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
