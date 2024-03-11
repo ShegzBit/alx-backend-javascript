a3dafef5bb21dc710e8ebc15ef8cf326e5a61e2b
ShegzBit <feranmiawotubo16@gmail.com>
2024-03-11 15:21:50 +0100

    solved task 1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,4 +1,4 @@
-export default function getListStudentIds (objArray) {
-    if (!(objArray instanceof Array)) { return []; }
-    return objArray.map((obj) => obj.id);
+export default function getListStudentIds(objArray) {
+  if (!(objArray instanceof Array)) { return []; }
+  return objArray.map((obj) => obj.id);
 }
