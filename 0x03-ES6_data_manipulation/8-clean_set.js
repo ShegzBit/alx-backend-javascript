96a7e50ffe0d004bc033033a16cac6aa62904d88
ShegzBit <feranmiawotubo16@gmail.com>
2024-03-12 23:50:47 +0100

    fixed lint-error task 8[2]

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
-  if (startString === '' || typeof(startString) !== 'string') { return ''; };
-  return [...set].filter((str) => str.startsWith(startString) && typeof(str) === 'string')
+  if (startString === '' || typeof (startString) !== 'string') { return ''; }
+  return [...set].filter((str) => str.startsWith(startString) && typeof (str) === 'string')
     .map((element) => element.slice(startString.length))
     .join('-');
 }
