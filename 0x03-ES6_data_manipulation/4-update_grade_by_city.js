40c89c5698263d652f47ef6347293554d54370a8
ShegzBit <feranmiawotubo16@gmail.com>
2024-03-12 22:34:30 +0100

    solved task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,12 +2,12 @@ export default function updateStudentGradeByCity(objArray, city, newGrades) {
   if (!(objArray instanceof Array && newGrades instanceof Array) || !(typeof (city) === 'string')) {
     return [];
   }
-  return objArray.filter((obj) => obj.location === city)
+  const arrayCopy = objArray;
+  return arrayCopy.filter((obj) => obj.location === city)
     .map((obj) => {
-      /* eslint-disable */
-      obj.grade = (newGrades.find((gradeObj) => gradeObj.studentId === obj.id))?.grade;
-      /* eslint-enable */
-      obj.grade = (typeof (obj.grade) === 'number') ? obj.grade : 'N/A';
-      return obj;
+      const newObj = obj;
+      const gradeObj = newGrades.find((gradeObj) => gradeObj.studentId === obj.id) || {};
+      newObj.grade = (gradeObj.grade) ? gradeObj.grade : 'N/A';
+      return newObj;
     });
 }
