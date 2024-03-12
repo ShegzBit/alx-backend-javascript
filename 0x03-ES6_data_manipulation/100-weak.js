eba3d425f377880c7ee488d724aa43dce97e3329
ShegzBit <feranmiawotubo16@gmail.com>
2024-03-13 00:38:55 +0100

    fixed task 11: added condition for endpoint doesn't exist in error throw check

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,6 +1,6 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint = { protocol: '', name: '' }) {
-  if (weakMap.get(endpoint) === 5) { throw new Error('Endpoint load is high'); }
+  if ((weakMap.get(endpoint) || 0) === 5) { throw new Error('Endpoint load is high'); }
   weakMap.set(endpoint, (weakMap.get(endpoint) || 0) + 1);
 }
