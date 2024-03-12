92e8246dbce543f49f10326161d52dc2c3176e89
ShegzBit <feranmiawotubo16@gmail.com>
2024-03-12 23:33:53 +0100

    fixed task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,4 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') { return ''; }
   return [...set].filter((element) => element.startsWith(startString))
     .map((element) => element.replace(startString, ''))
     .join('-');
