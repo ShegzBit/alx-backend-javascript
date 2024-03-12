888dac13c047d9266d8a95a59e44dd6a5e1dde40
ShegzBit <feranmiawotubo16@gmail.com>
2024-03-12 22:58:57 +0100

    solved task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,1 +1,6 @@
-#!/usr/bin/node
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const array = new Int8Array(buffer);
+  array[position] = value;
+  return new DataView(buffer);
+}
